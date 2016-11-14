23e20d7b7e5d291aa1ab8614ade1863d1a65e012
Marc DONVAL <marc.donval@jcdecaux.com>
2016-11-14 08:19:11 +0100

    .

--- it-keeps-core/src/main/java/org/ebaloo/itkeeps/core/ConfigFactory.java
+++ it-keeps-core/src/main/java/org/ebaloo/itkeeps/core/ConfigFactory.java
@@ -1,5 +1,13 @@
 package org.ebaloo.itkeeps.core;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
 import org.ebaloo.itkeeps.core.tools.MetricsFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,6 +40,7 @@ public final class ConfigFactory {
 			SLF4JBridgeHandler.install();
 
 			mainLogger = LoggerFactory.getLogger("Main");
+			
 		}
 		
 		
@@ -47,7 +56,8 @@ public final class ConfigFactory {
 		
 		mainLogger = LoggerFactory.getLogger(App.class.getName());
 		
-		mainLogger.info("Start log @" + logLevel);
+		mainLogger.info("mainlog start : " + logLevel);
+		mainLogger.info("version       : " + getManifestInfo());
     }
 	
     
@@ -127,6 +137,33 @@ public final class ConfigFactory {
 		
 		return defaultValue;
 	}
-    
+
 	
+	
+	public static String getManifestInfo() {
+	    Enumeration<URL> resEnum;
+	    try {
+	        resEnum = Thread.currentThread().getContextClassLoader().getResources(JarFile.MANIFEST_NAME);
+	        while (resEnum.hasMoreElements()) {
+	            try {
+	                URL url = (URL)resEnum.nextElement();
+	                InputStream is = url.openStream();
+	                if (is != null) {
+	                    Manifest manifest = new Manifest(is);
+	                    Attributes mainAttribs = manifest.getMainAttributes();
+	                    String version = mainAttribs.getValue("Implementation-Version");
+	                    if(version != null) {
+	                        return version;
+	                    }
+	                }
+	            }
+	            catch (Exception e) {
+	                // Silently ignore wrong manifests on classpath?
+	            }
+	        }
+	    } catch (IOException e1) {
+	        // Silently ignore wrong manifests on classpath?
+	    }
+	    return null; 
+	}
 }
